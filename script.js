6ff10fa084c1bb37f5e38207868fe9c2780bc822
douglasspeck <speck.empresarial@gmail.com>
2021-04-14 06:02:44 -0300

    v1.0

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-function dada(text) {
-    document.getElementById('first').appendClass('hidden');
-    document.getElementById('second').removeClass('hidden');
+function dada() {
+    var words = new Array();
+    words = document.getElementById('inputText').value.split(' ');
+    document.getElementById('first').classList.add('hidden');
+    document.getElementById('second').classList.remove('hidden');
+    shuffle(words);
+    for(let i=0; i<words.length; i++) {
+        var newCell = stringToHTML('<div class="cell" ondrop="drop(event)"><span class="word" draggable="true" ondragstart="drag(event)">' + words[i] + '</span></div>');
+        document.getElementById("words").append(newCell);
+    }
 }
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+  function stringToHTML(str) {
+
+	var dom = document.createElement('div');
+	dom.innerHTML = str;
+	return dom;
+
+};
